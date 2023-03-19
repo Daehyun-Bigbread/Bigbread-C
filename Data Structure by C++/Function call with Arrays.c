d8f7dfe36cdd0c3a3c6c76eaf476cbf9d2bd8d99
Daehyun Kim <bigdarkgold@gmail.com>
2023-03-19 22:00:42 +0900

    Create Function call with Arrays.c

---
+++ Data Structure by C++/Function call with Arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define ARRAY_SIZE	5
+
+void inputNumbers(int num[], int len);
+double computeAverage(int num[], int len);
+
+int main()
+{
+	int numbers[ARRAY_SIZE];
+	
+
+	inputNumbers(numbers, ARRAY_SIZE);
+	printf("average: %.3lf", computeAverage(numbers, ARRAY_SIZE));
+	
+	return 0;
+
+}
